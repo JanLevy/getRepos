1aab81f205c988884d3db18f230e58a5862a9819
JanLevy <HayashiMinoru@gmail.com>
2016-01-31 00:00:37 +0100

    Doplněn formulář na dotaz pro libovolného uživatele, základní výpis setřízených repozitářů.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//funkce pro ziskani JSON seznamu repozitaru z pozadovane URL
 function get_from_github($url) {
     try {
         $ch = curl_init();
@@ -24,11 +25,58 @@ function get_from_github($url) {
             E_USER_ERROR);
     }
 }
-
-$arr = get_from_github('https://api.github.com/users/jan-burianek/repos');
-$arr = json_decode($arr, true);
-
-
-foreach ($arr as $repo){
-    print $repo['name'] . "</br>";
+//pomocna funkce pro serazeni sestupne podle data vytvoreni repozitare
+function cmpcreateddown(array $a, array $b) {
+    if ($a['created_at'] > $b['created_at']) {
+        return -1;
+    } else if ($a['created_at'] < $b['created_at']) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
+?>
+<head>
+        <meta charset="UTF-8">
+        <title>Výpis repozitářů z Git Hubu</title>
+    </head>
+    <body>
+<?php
+
+if(isset($_GET['tofind'])){
+
+    $toFind = htmlspecialchars(trim($_GET['tofind']));
+    if (empty($toFind)){
+        $error_messages['tofind'] = "Název účtu k vyhledání chybí.";
+    }
+    if(empty($error_messages)) {
+        $arr = get_from_github('https://api.github.com/users/' . $toFind . '/repos');
+        $arr = json_decode($arr, true);
+        //prisla-li zpet message, jmeno nema repo
+        if(isset($arr['message'])){
+            print "Nebyly nalezeny žádné veřejné repozitáře k zadanému uživatelskému jménu.";
+        }
+        else{
+            //vyuziti pomocne funkce pro setrizeni $arr dle data vytvoreni rep
+            usort($arr, "cmpcreateddown");
+            foreach ($arr as $repo) {
+                print $repo['name'] . " " . $repo['created_at'] . "</br>";
+            }
+        }
+    }
+} ?>
+<div>
+<form method="get" action="index.php">
+    <input type="text" name="tofind" value="" id="get" />
+    <?php
+    if(isset($error_messages['tofind'])){
+        echo $error_messages['tofind'];
+    } else{
+        echo "<label for=\"get\">Zadejte název účtu k vyhledání</label>";
+    }
+    ?>
+    <input type="submit" value="Vyhledat!"/>
+</form>
+    </div>
+    </body>
+
